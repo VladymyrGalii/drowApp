ddb8577b3dceee5259529acd50249dbb4a748eff
PC_RUS <progressor1985@gmail.com>
2023-01-27 14:14:47 +0100

    start_01

---
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.drawingapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.drawingapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
